e00a536eb9bf5f75aaeca0496bf500d458845331
Pio Antonio <pioantonioperugini@gmail.com>
2021-05-14 10:34:26 +0200

    Aggiunto mapping rest per il magazziniere

--- CTT_DataBase/src/main/java/it/unisannio/ingegneriaDelSoftware/CttDataBaseRestApplication.java
+++ CTT_DataBase/src/main/java/it/unisannio/ingegneriaDelSoftware/CttDataBaseRestApplication.java
@@ -1,10 +1,19 @@
 package it.unisannio.ingegneriaDelSoftware;
 
+import it.unisannio.ingegneriaDelSoftware.DataManagers.MyMagazziniereCTTDataManager;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.ws.rs.ApplicationPath;
+
 @SpringBootApplication
-public class CttDataBaseRestApplication {
+@ApplicationPath("/rest")
+public class CttDataBaseRestApplication extends ResourceConfig {
+
+	public CttDataBaseRestApplication() {
+		register(MyMagazziniereCTTDataManager.class); //stiamo passando la classe da istanziare per il servizio
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CttDataBaseRestApplication.class, args);
--- CTT_DataBase/src/main/java/it/unisannio/ingegneriaDelSoftware/Interfaces/MagazziniereCTTDataManager.java
+++ CTT_DataBase/src/main/java/it/unisannio/ingegneriaDelSoftware/Interfaces/MagazziniereCTTDataManager.java
@@ -3,10 +3,35 @@ package it.unisannio.ingegneriaDelSoftware.Interfaces;
 import java.time.LocalDate;
 import it.unisannio.ingegneriaDelSoftware.Classes.Sacca;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public interface MagazziniereCTTDataManager {
 
-	void aggiuntaSaccaMagazzino(Sacca unaSacca, String enteDonatore);	
-	
-	String evasioneSacca(Sacca s, String enteRichiedente, LocalDate dataAffidamento);
-	
+	/**@param username username del magazziniere che si logga nel sistema
+	 * @param password  password del magazziniere che si logga nel sistema*/
+	//boolean login(String username, String password);
+
+	/**
+	 * @param seriale seriale della sacca da evadere
+	 * Metodo attivato dal magazziniere quando riceve una notifica evasione sacca
+	 * esso aggiorna i dati sacca e rimuove la sacca dal DB attivo
+	 * @return */
+	@PUT
+	@Path("/evasione/{seriale}")
+	Response evasioneSacca(@QueryParam("seriale") String seriale, String ente_richiedente);
+
+	/**@return  messaggio corretta aggiunta sacca*/
+	@POST
+	@Path("/aggiuntaSacca")
+	@Produces(MediaType.TEXT_PLAIN)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response aggiuntaSaccaMagazzino(@FormParam("gruppo_sanguigno") String gruppo_sanguigno,
+										   @FormParam("data_scadenza") String data_scadenza,
+										   @FormParam("data_produzione") String data_produzion,
+										   @FormParam("ente_donatore") String ente_donatore);
+
+
+
 }
